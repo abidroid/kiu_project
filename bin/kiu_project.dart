7fe8b3ec35694b7833192d4a3645b3107fc0cf48
Abid <abidroid1@gmail.com>
2024-07-09 15:41:03 +0500

    Initial commit

---
+++ bin/kiu_project.dart
@@ -0,0 +1,54 @@
+// first program
+
+/*
+  this is
+  our
+  first
+  program
+  at
+  KIU
+*/
+
+
+void main()
+{
+  // print('Hello World');
+  // print(5+5);
+  // print('5+5');
+  // print(5>3);
+
+
+  // Variables
+
+  // int totalStudents; // variable declaration
+  // totalStudents = 70; // initialization
+  // print(totalStudents);
+  //
+  // double gpa = 3;
+  // print(gpa);
+  // final double pi = 3.14;
+  // print(pi);
+  //
+  // print(pi);
+  //
+  // String universityName = "KIU";
+  // // String interpolation
+  // print('I read in $universityName');
+  //
+  // bool isMarried = false;
+  // bool feePaid = true;
+  //
+  // String firstName = 'Muhammad';
+  // print('First name is $firstName');
+  //
+
+  // Type inference
+  var country = 567;
+  //print(country.runtimeType);
+
+  dynamic value = 3.3;
+  print(value.runtimeType);
+
+  value = true;
+  print(value.runtimeType);
+}
